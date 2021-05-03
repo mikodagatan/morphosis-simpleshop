b6b3dec6717abf8afdd44e5aeb7607fbb5d36c2d
Miguel Dagatan <miguel.dagatan@gmail.com>
2021-05-04 00:28:11 +0800

    changed customer reference to customer id in migration

--- db/migrate/20210503161514_create_orders.rb
+++ db/migrate/20210503161514_create_orders.rb
@@ -3,10 +3,12 @@ class CreateOrders < ActiveRecord::Migration[6.0]
     create_table :orders do |t|
       t.string :reference_code
       t.decimal :total, precision: 14, scale: 2
-      t.references :customer, null: false, foreign_key: true
+      t.integer :customer_id, null: false, foreign_key: true
       t.references :address, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :orders, :customer_id, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_044155) do
+ActiveRecord::Schema.define(version: 2021_05_03_161809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "line1", null: false
+    t.string "line2"
+    t.string "city", null: false
+    t.string "state", null: false
+    t.string "postal_code"
+    t.bigint "user_id", null: false
+    t.bigint "region_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["region_id"], name: "index_addresses_on_region_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "reference_code"
+    t.decimal "total", precision: 14, scale: 2
+    t.integer "customer_id", null: false
+    t.bigint "address_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["address_id"], name: "index_orders_on_address_id"
+    t.index ["customer_id"], name: "index_orders_on_customer_id", unique: true
+  end
+
+  create_table "product_orders", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "subtotal", precision: 14, scale: 2
+    t.bigint "product_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_product_orders_on_order_id"
+    t.index ["product_id"], name: "index_product_orders_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title", null: false
@@ -58,5 +94,10 @@ ActiveRecord::Schema.define(version: 2021_05_03_044155) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "regions"
+  add_foreign_key "addresses", "users"
+  add_foreign_key "orders", "addresses"
+  add_foreign_key "product_orders", "orders"
+  add_foreign_key "product_orders", "products"
   add_foreign_key "products", "regions"
 end
